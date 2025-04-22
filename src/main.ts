ad9ac23fcb760aa7050c619e61524ef81a0a94c2
cpandares <pandaresocesar@gmail.com>
2025-04-22 10:43:03 -0400

    fix await app.listen in main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,27 +6,27 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
 
-  const logger = new Logger('Bootstrap');
+  const logger = new Logger('Main');
 
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    
     AppModule,
     {
       transport: Transport.TCP,
       options: {
-        port: envs.PORT,
+        port: envs.PORT
       }
     }
   );
-  
-  
-  app.useGlobalPipes(new ValidationPipe({     
-    whitelist: true,
-    forbidNonWhitelisted: true,
-   }));
- 
 
-  
-  logger.log(`Micro ServivesApplication is running on: ${envs.PORT}`);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  await app.listen();
+  logger.log(`Products Microservice running on port ${ envs.PORT }`);
+
 }
 bootstrap();
